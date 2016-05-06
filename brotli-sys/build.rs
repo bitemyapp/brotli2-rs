202cc5d9ce3f3fa841d7043173bb0d91480eeacc
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-05-06 12:52:04 +0100

    Modified the build script to try and locate the git executable on Windows.

--- brotli-sys/build.rs
+++ brotli-sys/build.rs
@@ -1,13 +1,77 @@
 extern crate gcc;
+#[cfg(windows)]
+extern crate winapi;
+#[cfg(windows)]
+extern crate winreg;
 
 use std::env;
 use std::process::Command;
 use std::path::Path;
 
+#[cfg(windows)]
+mod find_git {
+    use std::ffi::OsStr;
+    use std::io::Result;
+    use std::path::PathBuf;
+    use std::process::Command;
+    use winapi::HKEY;
+    use winreg::RegKey;
+    use winreg::enums::{HKEY_CURRENT_USER, HKEY_LOCAL_MACHINE, KEY_READ};
+
+    fn try_git_path<P: AsRef<OsStr>>(predefined_key: HKEY,
+                                     subkey_path: P,
+                                     value: P)
+                                     -> Option<PathBuf> {
+        let root = RegKey::predef(predefined_key);
+        if let Ok(subkey) = root.open_subkey_with_flags(subkey_path, KEY_READ) {
+            let subkey_value: Result<String> = subkey.get_value(value);
+            if let Ok(install_path) = subkey_value {
+                let binary_path = PathBuf::from(&install_path).join("bin").join("git.exe");
+                if Command::new(&binary_path).args(&["--version"]).status().is_ok() {
+                    return Some(binary_path);
+                }
+            }
+        }
+        None
+    }
+
+    pub fn git_path() -> PathBuf {
+        if Command::new("git").args(&["--version"]).status().is_ok() {
+            return PathBuf::from("git");
+        }
+        try_git_path(HKEY_LOCAL_MACHINE, "Software\\GitForWindows", "InstallPath").or_else(|| {
+            try_git_path(HKEY_CURRENT_USER,
+                         "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Git_is1",
+                         "InstallLocation")
+        }).or_else(|| {
+            try_git_path(HKEY_CURRENT_USER,
+                         "SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Git_is1",
+                         "InstallLocation")
+        }).or_else(|| {
+            try_git_path(HKEY_LOCAL_MACHINE,
+                         "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Git_is1",
+                         "InstallLocation")
+        }).or_else(|| {
+            try_git_path(HKEY_LOCAL_MACHINE,
+                         "SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Git_is1",
+                         "InstallLocation")
+        }).expect("Failed to locate Git executable.")
+    }
+}
+
+#[cfg(windows)]
+use find_git::git_path;
+
+#[cfg(not(windows))]
+fn git_path() -> ::std::path::PathBuf {
+    ::std::path::PathBuf::from("git")
+}
+
 fn main() {
     if !Path::new("brotli/.git").exists() {
-        let _ = Command::new("git").args(&["submodule", "update", "--init"])
-                                   .status();
+        let _ = Command::new(git_path())
+                    .args(&["submodule", "update", "--init"])
+                    .status();
     }
 
     let src = env::current_dir().unwrap();
